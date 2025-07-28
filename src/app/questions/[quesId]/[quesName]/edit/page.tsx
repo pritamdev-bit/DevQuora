e54efc97a87df6acd29a3f1c47878c8f58538540
Pritam Mandal <mandalpritam8617@gmail.com>
2025-07-28 12:24:41 +0530

    update 5

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,19 +1,50 @@
+// import { db, questionCollection } from "@/models/name";
+// import { databases } from "@/models/server/config";
+// import React from "react";
+// import EditQues from "./Editques";
+// // import { PageProps } from "./types"; // or wherever you defined it
+
+// // const EditQuestionPage = async ({ params }: { params: { quesId: string; quesName: string } }) => {
+// //     const question = await databases.getDocument(db, questionCollection, params.quesId);
+
+// //     return <EditQues question={question} />;
+// // };
+
+
+// interface PageProps {
+//   params: {
+//     quesId: string;
+//     quesName: string;
+//   };
+// }
+
+// const EditQuestionPage = async ({ params }: PageProps) => {
+//   const question = await databases.getDocument(db, questionCollection, params.quesId);
+//   return <EditQues question={question} />;
+// };
+
+// export default EditQuestionPage;
+
+// app/questions/[quesId]/[quesName]/edit/page.tsx
+
 import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./Editques";
 
-type CustomPageProps = {
-  params: {
-    quesId: string;
-    quesName: string;
-  };
+const EditQuestionPage = async ({
+  params,
+}: {
+  params: { quesId: string; quesName: string };
+}) => {
+  const question = await databases.getDocument(
+    db,
+    questionCollection,
+    params.quesId
+  );
+
+  return <EditQues question={question} />;
 };
 
-const Page = async ({ params }: CustomPageProps) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
+export default EditQuestionPage;
 
-    return <EditQues question={question} />;
-};
-
-export default Page;
